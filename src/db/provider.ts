a20fa4abfed1f41ea9821ce72144d4a34b15ff4d
koss <jobs.koss@gmail.com>
2025-07-03 22:03:40 +0300

    feat: added new prisma client

---
+++ src/db/provider.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from './client'
+import { config } from '@config'
+
+export const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: config.postgres.uri,
+    },
+  },
+})
+
+export default prisma
