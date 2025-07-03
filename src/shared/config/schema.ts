39cbd2ab1008c7826645377a0bc9846232f7f58c
koss <jobs.koss@gmail.com>
2025-07-04 02:31:35 +0900

    feat: added config and validation for .env variables

---
+++ src/shared/config/schema.ts
@@ -0,0 +1,36 @@
+import Joi from 'joi'
+
+export const envSchema = Joi.object({
+  // Основные настройки
+  NODE_ENV: Joi.string()
+    .valid('development', 'production', 'test')
+    .default('development'),
+
+  PORT: Joi.number().port(),
+
+  CORS_ORIGIN: Joi.string().default('*'),
+
+  // Postgres
+  POSTGRES_DB_NAME: Joi.string(),
+  POSTGRES_HOST: Joi.string(),
+  POSTGRES_USER: Joi.string(),
+  POSTGRES_PASS: Joi.string(),
+  POSTGRES_PORT: Joi.string(),
+
+  // Email
+  // SMTP_HOST: Joi.string().when('NODE_ENV', {
+  //   is: 'production',
+  //   then: Joi.required(),
+  //   otherwise: Joi.optional(),
+  // }),
+  // SMTP_PORT: Joi.number().port().default(587),
+  // SMTP_USER: Joi.string().email().optional(),
+  // SMTP_PASSWORD: Joi.string().optional(),
+
+  // Other settings
+  // LOG_LEVEL: Joi.string()
+  //   .valid('error', 'warn', 'info', 'debug')
+  //   .default('info'),
+})
+  // .unknown() // allow variables that not defined in schema
+  .required()
