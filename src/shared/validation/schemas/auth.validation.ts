15de3a27d90eed65b1346ebbbaae7a818ea4afec
koss <jobs.koss@gmail.com>
2025-07-08 08:39:26 +0300

    feat: updated the validation scheme for authentication, added new domains and minimum password length

--- src/shared/validation/schemas/auth.validation.ts
+++ src/shared/validation/schemas/auth.validation.ts
@@ -1,20 +1,27 @@
+import { PASSWORD_MIN_LENGTH } from '@shared/constants'
 import Joi from 'joi'
 
 export const authSchema = Joi.object({
   body: Joi.object({
     email: Joi.string()
-      .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'ru'] } })
+      .email({
+        minDomainSegments: 2,
+        tlds: { allow: ['com', 'net', 'org', 'ru', 'ua', 'kz', 'by'] },
+      })
       .required()
       .messages({
         'string.base': '{#label} must be a string',
         'string.email': '{#label} is not valid',
         'any.required': '{#label} is required',
       }),
-    password: Joi.string().min(8).required().messages({
-      'string.base': '{#label} must be a string',
-      'string.min': '{#label} is too short',
-      'any.required': '{#label} is required',
-    }),
+    password: Joi.string()
+      .min(PASSWORD_MIN_LENGTH)
+      .required()
+      .messages({
+        'string.base': '{#label} must be a string',
+        'string.min': `{#label} must be at least ${PASSWORD_MIN_LENGTH} characters long`,
+        'any.required': '{#label} is required',
+      }),
   }),
   params: Joi.object({}),
   query: Joi.object({}),
