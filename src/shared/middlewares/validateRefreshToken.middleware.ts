589248612c4b395a78540a70b9242acf11cdbded
koss <jobs.koss@gmail.com>
2025-07-08 08:51:07 +0300

    feat: added middleware for refresh token validation

---
+++ src/shared/middlewares/validateRefreshToken.middleware.ts
@@ -0,0 +1,20 @@
+import { Context, Next } from 'koa'
+import { SessionService } from '@/services'
+import { TokenType } from '@shared/types'
+import { ApiError } from '../api'
+
+export async function validateRefreshTokenMiddleware(ctx: Context, next: Next) {
+  const refreshToken = ctx.cookies.get('refreshToken')
+
+  if (!refreshToken) {
+    throw ApiError.Forbidden('Refresh token is required')
+  }
+
+  ctx.tokenPayload = SessionService.validateToken(
+    refreshToken,
+    TokenType.REFRESH
+  )
+  ctx.refreshToken = refreshToken
+
+  await next()
+}
