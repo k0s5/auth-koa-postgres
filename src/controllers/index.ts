6b8f646a74fbde4ef0a6fae931445e2f24fbc602
koss <jobs.koss@gmail.com>
2025-07-08 09:07:02 +0300

    feat: methods for registering, logging in, updating and logging out a user using authentication and session services have been implemented

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,20 +1,84 @@
-import AuthService from '@/services'
+import { Context } from 'koa'
+import { AuthService, SessionService } from '@services'
+import { ISignInPayload, ISignUpPayload } from '@shared/types'
+import { ApiError } from '@/shared/api'
+import bcrypt from 'bcryptjs'
+import { userResponseFilter } from '@/db/filters/user.filter'
+import { omit } from '@/shared/utils'
 
 class AuthController {
-  static async signup() {
-    await AuthService.signup()
+  static async signup(ctx: Context) {
+    const { email, password, terms } = ctx.request.body as ISignUpPayload
+
+    //todo add user fingerprint
+    const newUser = await AuthService.createUser({ email, password })
+
+    const { accessToken, refreshToken } = await SessionService.create(ctx, {
+      email,
+      userId: newUser.id,
+      username: newUser.username,
+    })
+
+    SessionService.setRefreshTokenCookie(ctx, refreshToken)
+
+    ctx.body = {
+      user: newUser,
+      accessToken,
+    }
   }
 
-  static async signin() {
-    await AuthService.signin()
+  static async signin(ctx: Context) {
+    const { email, password } = ctx.request.body as ISignInPayload
+
+    const user = await AuthService.getUser(email)
+
+    const isPasswordValid = await bcrypt.compare(password, user.passwordHash)
+
+    if (!isPasswordValid) {
+      throw ApiError.BadRequest(`Incorrect password`)
+    }
+
+    const { accessToken, refreshToken } = await SessionService.create(ctx, {
+      email,
+      userId: user.id,
+      username: user.username,
+    })
+
+    SessionService.setRefreshTokenCookie(ctx, refreshToken)
+
+    ctx.body = {
+      user: omit(user, ['passwordHash']),
+      accessToken,
+    }
   }
 
-  static async refresh() {
-    await AuthService.refresh()
+  static async refresh(ctx: Context) {
+    const user = await AuthService.getUser(
+      ctx.tokenPayload!.email,
+      userResponseFilter
+    )
+
+    const { accessToken, refreshToken: newRefreshToken } =
+      SessionService.generateTokens(ctx.tokenPayload!)
+
+    await SessionService.update(ctx.refreshToken!, { newRefreshToken })
+
+    SessionService.setRefreshTokenCookie(ctx, newRefreshToken)
+
+    ctx.body = {
+      user,
+      accessToken,
+    }
   }
 
-  static async signout() {
-    await AuthService.signout()
+  static async signout(ctx: Context) {
+    await SessionService.delete(ctx.refreshToken!)
+
+    SessionService.setRefreshTokenCookie(ctx, null, {
+      expires: new Date(0),
+    })
+
+    ctx.body = 'User signout successfully'
   }
 }
 
