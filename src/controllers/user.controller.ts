112add3de5691d23202f58c9856e41a10e706a42
koss <jobs.koss@gmail.com>
2025-07-10 19:36:15 +0300

    refactor: changed argument type for me method

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,9 +1,9 @@
 import type { Context } from 'koa'
-import type { IValidateRefreshTokenContext } from '@shared/types'
+import type { IAccessTokenVerificationContext } from '@shared/types'
 import { UserService } from '@services'
 
 export class UserController {
-  static async me(ctx: Context & IValidateRefreshTokenContext) {
+  static async me(ctx: Context & IAccessTokenVerificationContext) {
     const user = await UserService.getUserById(ctx.tokenPayload.userId)
     ctx.body = user
   }
