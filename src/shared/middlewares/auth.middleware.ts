f84dd8c63d7c5c62ce3a71e2b1e71900f90c88ba
koss <jobs.koss@gmail.com>
2025-07-10 19:31:36 +0300

    feat: added token payload to koa context

--- src/shared/middlewares/auth.middleware.ts
+++ src/shared/middlewares/auth.middleware.ts
@@ -3,14 +3,17 @@ import { SessionService } from '@/services'
 import { TokenType } from '@shared/types'
 import { ApiError } from '../api'
 
-export async function authMiddleware(ctx: Context, next: Next) {
+export async function accessTokenVerification(ctx: Context, next: Next) {
   const { authorization } = ctx.request.headers
 
   if (!authorization) {
     throw ApiError.Forbidden('Authorization token is not provided')
   }
 
-  SessionService.validateToken(authorization, TokenType.ACCESS)
+  // Split token from Bearer prefix
+  const token = authorization.split(' ')[1]
+
+  ctx.tokenPayload = SessionService.validateToken(token, TokenType.ACCESS)
 
   await next()
 }
